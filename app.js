5f6bf0327512f103aa370b85d1050d9979ea11d8
K1kva2006 <115255875+K1kva2006@users.noreply.github.com>
2023-08-07 21:40:35 +0400

    1.1

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const container = document.getElementById("container");
-const generate = document.getElementById("generate")
+const generate = document.getElementById("generate");
+generate.style.display = "none";
 
 let url =
     "https://api.thecatapi.com/v1/images/search?format=json&limit=1' --header 'Content-Type: application/json' --header 'x-api-key: example-api-key";
@@ -8,14 +9,20 @@ fetch(url)
     .then((getData) => {
         getData.forEach((element) => {
             const img = document.createElement("img");
+
             img.setAttribute("src", element.url);
             img.setAttribute("alt", "cat");
             container.appendChild(img);
             img.classList.add("img-container");
+            container.insertBefore(img, generate);
         });
     })
     .catch((err) => console.log(err));
 
+
+setTimeout(() => {
+    generate.style.display = "block"
+}, 1500)
 generate.addEventListener("click", () => {
     window.location.reload();
-})
+});
